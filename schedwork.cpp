3648a84c70861fe4a6827f41cf30963fd0f50144
Ryan Treacy <rtreacy@usc.edu>
2025-04-24 21:35:54 +0000

    Finished HW5

--- schedwork.cpp
+++ schedwork.cpp
@@ -17,11 +17,13 @@ using namespace std;
 // a constant that can be used to indicate an INVALID 
 // worker ID if that is useful to your implementation.
 // Feel free to not use or delete.
-static const Worker_T INVALID_ID = (unsigned int)-1;
+//static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+// ok we will add a prototype to have the recursive function that fills in the schedule and does the backtracking
+//void fill
+bool fillIn(size_t day, size_t slot, const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, std::vector<int>& worked, DailySchedule& sched);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -37,9 +39,52 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    // need to store the number of days and the number of workers we have and base it on the input matrix
+    size_t daysCount = avail.size();
+    size_t workerCount = avail[0].size();
+  // initialize sched to have the a row count that is the number of days and start them with an empty vector that we put workers into 
+    sched = DailySchedule(daysCount, std::vector<Worker_T>());
+  // create a vector to track how many shifts each worker has been assigned , where worked[i] stores the # of shifts for worker i
+    std::vector<int>worked(workerCount, 0);
+  // call recursiviley starting with day 0, slot 0 which is the first worker to assign on the first day
+    return fillIn(0,0,avail,dailyNeed, maxShifts, worked, sched);
+}
 
+// implement 
+bool fillIn(size_t day, size_t slot, const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, std::vector<int>& worked, DailySchedule& sched)
+{
+  // base case will be if the schedle is filled
+  if(day == sched.size())
+  {
+    return true;
+  }
 
+  // for a slot check every worker
+  for(size_t worker = 0; worker<avail[0].size(); ++worker)
+  {
+    // need to check three things to assign: if theyre available that day, if they havent hit max shifts, and if theyr enot already scheduled 
+    if(avail[day][worker] && worked[worker] < (int)maxShifts && find(sched[day].begin(), sched[day].end(), worker) == sched[day].end())
+    {
+      sched[day].push_back(worker); // temporarily assigns the worker to the schedule
+      worked[worker]++; // updates the shift count
 
+      size_t nextDay = day;
+      //size_t nextWorker = 
+      size_t nextSlot = slot+1;
 
+      if(nextSlot == dailyNeed) // if we have filled the daily needs of workers for the current day, we move to the next day and reset it to 0
+      {
+        nextDay++;
+        nextSlot = 0;
+      }
+      if(fillIn(nextDay, nextSlot, avail, dailyNeed, maxShifts, worked, sched)) // recursivley fill the rest of the schedule 
+      {
+        return true; // true!
+      }
+      sched[day].pop_back(); // if the recursion fails, call backtrack removing the worker from the current day and decrementing their shift count 
+      worked[worker]--;
+    }
+  }
+  return false; // further backtracking
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,6 +13,9 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+// ok we'll do a prototype to create
+// need a string for the partially built word, a string for the fixed letters and -, the floats that have to be used, and the current index, and the result
+void create(std::string partialWord, const std::string& in, std::string floating, int position, const std::set<std::string>&dict, std::set<std::string>&final);
 
 
 // Definition of primary wordle function
@@ -22,7 +25,80 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    std::set<std::string> final;
+    create("", in, floating, 0, dict, final);
+    return final;
 
 }
 
 // Define any helper functions here
+void create(std::string partialWord, const std::string& in, std::string floating, int position, const std::set<std::string>&dict, std::set<std::string>&final)
+{
+  // ok this is gonna be recursive
+  // so base case first!! 
+  // base case will be if the word is fully correct
+  if(position == in.size())
+  {
+    if(floating.empty() && dict.find(partialWord) != dict.end()) // only adds the word when all the floating letters have been used and it exists in the dictionary
+    {
+      final.insert(partialWord);
+    }
+    return;
+  }
+  // second case is when we need to fill in a - 
+  if(in[position] != '-')
+  {
+    // recursively call the function to use the floating letter and go forward
+    create(partialWord + in[position], in, floating, position+1, dict, final);
+  }
+  else // this will cqtch when its bnlank and we have got to fill it
+  {
+    std::set<char>openLetters;
+    
+    //if(floating.size() > 0) // we have to start with these letters first
+    //{
+    for(char c : floating)
+    {
+      openLetters.insert(c);
+    }
+    // then try all the other letters
+    //if((int)(in.size()- position) > (int)floating.size())
+    if((in.size() - position) > floating.size())
+    {
+      for(char a = 'a'; a <= 'z'; ++a)
+      {
+        openLetters.insert(a);
+      }
+    }
+    if(floating.empty())
+    {
+      for(char c = 'a'; c <= 'z'; ++c)
+      {
+        openLetters.insert(c);
+      }
+    }
+   // else
+   //{
+    //  for(char a = 'a'; a<= 'z'; ++a)
+     // {
+      //  openLetters.insert(a);
+     // }
+   // }
+    for(char b : openLetters)
+    {
+      std::string newFloat = floating;
+       // size_t position = newFloat.find(b);
+      size_t floatPosition = newFloat.find(b);
+        //if(position != std::string::npos)
+      if(floatPosition != std::string::npos)
+      {
+          //newFloat.erase(position, 1);
+        newFloat.erase(floatPosition, 1);
+      }
+      create(partialWord+b, in, newFloat, position+1, dict, final);
+    }
+    //}
+  }
+}
+
+
